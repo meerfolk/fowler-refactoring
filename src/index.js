0c00d775edb756fc037aa6c0d1aaa3e3e977499a
meerfolk <meerfolk@gmail.com>
2021-10-16 17:47:37 +0500

    Chapter 1 calculate amount in separate function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,36 @@ const { equal } = require('assert');
 const plays = require('../data/plays.json');
 const invoices = require('../data/invoices.json');
 
+function amountFor(aPerformance, play) {
+    let result = 0;
+
+    switch (play.type) {
+        case 'tragedy':
+            result = 40000;
+
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
+            }
+
+            break;
+        
+        case 'comedy':
+            result = 30000;
+
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+            }
+
+            result += 300 * aPerformance.audience;
+            break;
+        
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return result;
+}
+
 function statement(invoices, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
@@ -14,31 +44,7 @@ function statement(invoices, plays) {
 
     for (let perf of invoices.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case 'tragedy':
-                thisAmount = 40000;
-
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-
-                break;
-            
-            case 'comedy':
-                thisAmount = 30000;
-
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-
-                thisAmount += 300 * perf.audience;
-                break;
-            
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play);
 
         volumeCredits += Math.max(perf.audience - 30, 0);
 
