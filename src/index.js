f6395f8cc4efed13b06197ffb49c895ecdb15872
meerfolk <meerfolk@gmail.com>
2021-10-26 20:41:37 +0500

    Chapter 4 add border tests

--- src/index.js
+++ src/index.js
@@ -128,17 +128,48 @@ describe('province', function() {
 
     beforeEach(function() {
         asia = new Province(sampleProvinceData());
-    })
+    });
 
     it('shortfall', function() {
         expect(asia.shortfall).equal(5);
     });
     it('profit', function() {
         expect(asia.profit).equal(230);
-    })
+    });
     it('change production', function() {
         asia.producers[0].production = 20;
         expect(asia.shortfall).equal(-6);
         expect(asia.profit).equal(292);
-    })
+    });
+    it('zero demand', function() {
+        asia.demand = 0;
+        expect(asia.shortfall).equal(-25);
+        expect(asia.profit).equal(0);
+    });
+    it('negative demand', function() {
+        asia.demand = -1;
+        expect(asia.shortfall).equal(-26);
+        expect(asia.profit).equal(-10);
+    });
+});
+
+describe('no producers', function() {
+    let noProducers;
+
+    beforeEach(function() {
+        const data = {
+            name: 'No producers',
+            producers: [],
+            demand: 30,
+            price: 20,
+        }
+        noProducers = new Province(data);
+    });
+
+    it('shortfall', function() {
+        expect(noProducers.shortfall).equal(30);
+    });
+    it('profit', function() {
+        expect(noProducers.profit).equal(0);
+    });
 });
